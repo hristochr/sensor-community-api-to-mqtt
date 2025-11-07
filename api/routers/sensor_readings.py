991b5e34996d8a226d83dfd3fe337f3afde8b44a
Hristo Hristov <YOUR_EMAIL>
2025-11-07 10:01:21 +0100

    added sql background task

--- api/routers/sensor_readings.py
+++ api/routers/sensor_readings.py
@@ -5,12 +5,21 @@ from fastapi import APIRouter, HTTPException, BackgroundTasks, Depends
 from ..models.air_data import SensorReading
 from ..dependencies import verify_credentials
 from ..publisher import MQTTPublisher
+from ..sql_client import SQLClient
+from rds import RDSConfig
+from ..transformer import SensorDataTransformer
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = APIRouter(include_in_schema=True)
 mqtt_publisher = MQTTPublisher()
+
+rds_config = RDSConfig()
+sql_agent = SQLClient(rds_config=rds_config)
+
+transformer = SensorDataTransformer()
 
 
 @app.post("/sensor-data",  response_model=None)
@@ -30,7 +39,7 @@ async def receive_sensor_data(
     """
     try:
         # convert Pydantic model to dict
-        sensor_data = data.model_dump()
+        result_dict = transformer.transform_to_dict(data)
 
         try:
             background_tasks.add_task(
@@ -38,7 +47,11 @@ async def receive_sensor_data(
             )
             background_tasks.add_task(
                 mqtt_publisher.publish_payload,
-                sensor_data
+                result_dict
+            )
+            background_tasks.add_task(
+                sql_agent.store_payload,
+                result_dict
             )
         finally:
             mqtt_publisher.disconnect()
@@ -46,7 +59,7 @@ async def receive_sensor_data(
         return {
             "status": "success",
             "message": f"Sensor data received from {username} and will be published to MQTT",
-            "data": sensor_data
+            "data": result_dict
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing sensor data: {str(e)}")
